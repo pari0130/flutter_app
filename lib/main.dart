5070d6d351173240cc959b03e47f75eb2952fda0
donghwi <pari0130@gmail.com>
2020-04-26 16:22:04 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/screen/home_screen.dart';
+import 'package:flutterapp/widget/bottom_bar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TabController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'ChoFlix',
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.black,
+          accentColor: Colors.white,
+        ),
+        home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            body: TabBarView(
+              physics: NeverScrollableScrollPhysics(), // 터치 스크롤을 막고 하단 탭 선택으로 이동되도록 함
+              children: <Widget>[
+                HomeScreen(),
+                Container(
+                  child: Center(
+                    child: Text('search'),
+                  ),
+                ),
+                Container(
+                  child: Center(
+                    child: Text('save'),
+                  ),
+                ),
+                Container(
+                  child: Center(
+                    child: Text('more'),
+                  ),
+                ),
+              ],
+            ),
+            bottomNavigationBar: Bottom(),
+          ),
+        ));
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TopBar();
+  }
+}
+
+class TopBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        children: <Widget>[
+          Image.asset(
+            'images/Netflix_Symbol_RGB.png',
+            fit: BoxFit.contain,
+            height: 60,
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              'TV 프로그램',
+              style: TextStyle(fontSize: 14),
+          ),)
+        ],
+      ),
+    );
+  }
+}
